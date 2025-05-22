6e170da1fc8704a81d4393f462b80ef05a20fcfb
yunachooi <glasodyyn@gmail.com>
2025-05-22 12:22:34 +0900

    [ynchoi] branch, member, reservation dto

--- roomEscape/src/main/java/com/example/roomEscape/dto/MemberDTO.java
+++ roomEscape/src/main/java/com/example/roomEscape/dto/MemberDTO.java
@@ -1,5 +1,14 @@
 package com.example.roomEscape.dto;
 
-public class MemberDTO {
+import lombok.Data;
 
+@Data
+public class MemberDTO {
+	private String member_id;
+	private String password;
+	private String name;
+	private String phone;
+	private String email;
+	private String reg_date;
+	private String role;
 }
