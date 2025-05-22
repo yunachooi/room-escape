6b4a0041c74a6ee94e49a96ca9d5752b2145f17e
yunachooi <glasodyyn@gmail.com>
2025-05-22 10:01:41 +0900

    [ynchoi] init

---
+++ roomEscape/src/main/java/com/example/roomEscape/RoomEscapeApplication.java
@@ -0,0 +1,13 @@
+package com.example.roomEscape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomEscapeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomEscapeApplication.class, args);
+	}
+
+}
---
+++ roomEscape/src/test/java/com/example/roomEscape/RoomEscapeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.roomEscape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomEscapeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
