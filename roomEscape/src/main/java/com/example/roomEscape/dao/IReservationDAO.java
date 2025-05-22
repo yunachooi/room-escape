3d80fd4b412c8b9af5d3a77a82cfd0dbde0a98db
yunachooi <glasodyyn@gmail.com>
2025-05-22 16:52:41 +0900

    [ynchoi] admin - reservation Task

--- roomEscape/src/main/java/com/example/roomEscape/dao/IReservationDAO.java
+++ roomEscape/src/main/java/com/example/roomEscape/dao/IReservationDAO.java
@@ -1,5 +1,16 @@
 package com.example.roomEscape.dao;
 
-public class IReservationDAO {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.example.roomEscape.dto.ReservationCountDTO;
+import com.example.roomEscape.dto.ReservationDTO;
+
+@Mapper
+public interface IReservationDAO {
+	List<ReservationDTO> reservationSelect();
+	List<ReservationDTO> findReservationName(@Param("NAME") String NAME);
+	List<ReservationCountDTO> getReservationCountByDate(String find_date);
 }
---
+++ roomEscape/src/main/java/com/example/roomEscape/dto/ReservationCountDTO.java
@@ -0,0 +1,9 @@
+package com.example.roomEscape.dto;
+
+import lombok.Data;
+
+@Data
+public class ReservationCountDTO {
+	private String branchName;
+	private int count;
+}
