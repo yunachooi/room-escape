4b0822321e80993ca3ecbbf922fe00eba1e4362e
s-hyunn <cktngus4@naver.com>
2025-05-28 01:49:09 +0900

    [sh] 검색옵션 안나오던것 수정, 리팩토링

--- roomEscape/src/main/java/com/example/roomEscape/controller/HomeController.java
+++ roomEscape/src/main/java/com/example/roomEscape/controller/HomeController.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.roomEscape.dao.IBranchDAO;
 import com.example.roomEscape.dao.IThemeDAO;
+import com.example.roomEscape.dao.IThemeTypeDAO;
 import com.example.roomEscape.dto.ThemeDTO;
 
 import lombok.RequiredArgsConstructor;
@@ -16,11 +18,18 @@ import lombok.RequiredArgsConstructor;
 public class HomeController {
 
     private final IThemeDAO iThemeDAO;
+    private final IBranchDAO branchDAO;
+    private final IThemeTypeDAO themeTypeDAO;
 
     @GetMapping("/")
     public String home(Model model) {
         List<ThemeDTO> themes = iThemeDAO.selectAll();
         model.addAttribute("themeList", themes);
+
+        // ✅ 이거 추가!
+        model.addAttribute("branches", branchDAO.selectAll());
+        model.addAttribute("types", themeTypeDAO.selectAll());
+
         return "index";
     }
 }
